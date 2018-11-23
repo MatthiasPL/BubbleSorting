8beada3767c42a0f38d60404b9125d38dceb9c01
MatthiasPL <smateusz96@gmail.com>
2018-11-23 20:52:20 +0100

    [to-do] loop and pushing the biggest variable to end of the array

--- SortPrzezPodstawianie/SortPrzezPodstawianie/Program.cs
+++ SortPrzezPodstawianie/SortPrzezPodstawianie/Program.cs
@@ -8,19 +8,34 @@ namespace SortPrzezPodstawianie
 {
     class Program
     {
-        public static List<int> array = new List<int>{ 3, 5, 2, 1, 6, 7 };
+        public static int[] tosort = { 1, 2, 3, 4, 5,6,7,8,9,10,11,12,13,14,15,16,17,18,19, 20, 21, 22, 23 };
+        public static List<int> array = tosort.ToList();
         public static List<int> indexes = new List<int>();
         public static int steps = 0;
         public static int step = 0;
 
         static void Main(string[] args)
         {
-            FindMaximalValue();
-            for(int i = 0; i < array.Count(); i++)
+            for(int i =0; i< array.Count(); i++)
             {
-                Console.WriteLine(array[i]);
+                indexes.Add(i);
             }
+
+            FindMaximalValue();
+            for (int i = 0; i < array.Count(); i++)
+            {
+                Console.WriteLine(indexes[i]);
+            }
+            ResetIndexes();
             Console.ReadKey();
+        }
+
+        private static void ResetIndexes()
+        {
+            for (int i = 0; i < array.Count(); i++)
+            {
+                indexes[i]=i;
+            }
         }
 
         private static void Initialize()
@@ -37,17 +52,14 @@ namespace SortPrzezPodstawianie
 
             for(step = 1; step <= steps; step++)
             {
-                //Console.WriteLine(step);
-                Parallel.For(0, array.Count()/(int)Math.Pow(2, step), CompareSwap);
+                Parallel.For(0, (int)Math.Ceiling(Math.Log(2, array.Count()))+50, CompareSwap);
             }
-
-            indexes.Insert(0, array[0]);
         }
 
         private static void CompareSwap(int i)
         {
             int index1 = i * (int)Math.Pow(2, step);
-            int index2 = 1 + i * (int)Math.Pow(2, step); //tu jest źle
+            int index2 = (int)Math.Pow(2, step - 1) + i * (int)Math.Pow(2, step);
 
             if (index2<array.Count())
             {
@@ -60,6 +72,10 @@ namespace SortPrzezPodstawianie
                     int temp = array[index1];
                     array[index1] = array[index2];
                     array[index2] = temp;
+
+                    int tempindex = index1;
+                    indexes[index1] = indexes[index2];
+                    indexes[index2] = tempindex;
                 }
             }
         }
